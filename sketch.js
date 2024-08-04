a07d3eb0eaebc2662c6623afb245b341952d8f3f
Stéphane FRACHE <eregrith@gmail.com>
2024-08-05 01:39:26 +0200

    Added mutations

--- sketch.js
+++ sketch.js
@@ -40,7 +40,10 @@ let h = 20;
 let pixelSize = 5;
 let img = 0;
 let range = 2;
-let radio2;
+let radio_display;
+let checkbox_highlight;
+let mutation_amount = 10;
+let heavy_mutation_rate = 0.1;
 
 function setup() {
   for (let y = 0; y < h; y++) {
@@ -48,21 +51,35 @@ function setup() {
       programs.push(new Program(x, y));
     }
   }
+
+  let replicatorString = "[[{.>]-]                                                ]-]>.{[[";
+  for (let i = 0; i < replicatorString.length; i++) {
+    programs[0].contents[i].c = replicatorString.charCodeAt(i);
+  }
+
   console.log(programs);
   createCanvas(w*8*pixelSize, h*8*pixelSize);
   colorMode(HSB, 255);
   img = createImage(w*8*pixelSize, h*8*pixelSize);
   img.loadPixels();
-  radio2 = createRadio();
-  radio2.option('Instructions');
-  radio2.option('Identities');
-  radio2.selected('Instructions');
+  
+  radio_display = createRadio();
+  radio_display.option('Instructions');
+  radio_display.option('Identities');
+  radio_display.selected('Instructions');
+  
+  checkbox_highlight = createCheckbox('Highlight taken', false);
+
+  programFocus = createDiv('');
+  programFocus.style('font-size', '36px');
+  programFocus.style('font-family', 'Consolas');
+  programFocus.html('Program at mouse: ');
 }
 
 function setColorFromChar(c, img, x, y, taken) {
   let hue = c;
-  let brightness = /* taken ? 70 : */ 100;
-  let saturation = /* taken ? 70 : */ 100;
+  let brightness = (taken || !checkbox_highlight.checked()) ? 100 : 70;
+  let saturation = (taken || !checkbox_highlight.checked()) ? 100 : 70;
   if (c == 0) {
     brightness = 0;
     saturation = 0;
@@ -81,8 +98,8 @@ function setColorFromChar(c, img, x, y, taken) {
 
 function setColorFromId(id, img, x, y, taken) {
   let hue = lerp(0, 255, id / token_id);
-  let brightness = /* taken ? */ 255 /* : 100 */;
-  let saturation = /* taken ? */ 255 /* : 100 */;
+  let brightness =  (taken || !checkbox_highlight.checked()) ? 255 : 100;
+  let saturation =  (taken || !checkbox_highlight.checked()) ? 255 : 100;
   for (let pxy = 0; pxy < pixelSize; pxy++) {
     for (let pxx = 0; pxx < pixelSize; pxx++) {
       img.set(x*pixelSize + pxx, y*pixelSize + pxy, color(hue, saturation, brightness));
@@ -102,7 +119,7 @@ function exec_and_split(progA, progB) {
   let head0 = 0;
   let head1 = 0;
   let ip = 0;
-  for (let i = 0; i < pow(8, 2) * 4; i++) {
+  for (let i = 0; i < pow(8, 2) * 20; i++) {
     let c = char(tape[ip].c);
     if ("<>{}-+.,[]".indexOf(c) == -1) {
       ip = (ip + 1) % tape.length;
@@ -114,26 +131,28 @@ function exec_and_split(progA, progB) {
     if (c == '}') head1 = (head1 + 1) % tape.length;
     if (c == '-') tape[head0].c = (tape[head0].c + 255) % 256;
     if (c == '+') tape[head0].c = (tape[head0].c + 1) % 256;
-    if (c == '.') tape[head1] = tape[head0];
-    if (c == ',') tape[head0] = tape[head1];
+    if (c == '.') tape[head1] = new Token(tape[head0].id, tape[head0].c);
+    if (c == ',') tape[head0] = new Token(tape[head1].id, tape[head1].c);
     if (c == '[' && tape[head0].c == 0) {
-      let next = ip + 1;
+      let next = ip;
       let depth = 1;
-      while (depth != 0 && next != ip) {
+      while (depth != 0) {
         next = (next + 1) % tape.length;
-        if (char(tape[(ip + next) % tape.length].c) == '[') depth++;
-        if (char(tape[(ip + next) % tape.length].c) == ']') depth--;
+        if (next == ip) break;
+        if (char(tape[next].c) == '[') depth++;
+        if (char(tape[next].c) == ']') depth--;
       }
       if (next == ip) break;
       ip = next;
     }
-    if (c.c == ']' && tape[head0].c != 0) {
-      let next = ip - 1;
+    if (c == ']' && tape[head0].c != 0) {
+      let next = ip;
       let depth = 1;
-      while (depth != 0 && next != ip) {
+      while (depth != 0) {
         next = (next - 1 + tape.length) % tape.length;
-        if (char(tape[(ip + next) % tape.length].c) == ']') depth++;
-        if (char(tape[(ip + next) % tape.length].c) == '[') depth--;
+        if (next == ip) break;
+        if (char(tape[next].c) == ']') depth++;
+        if (char(tape[next].c) == '[') depth--;
       }
       if (next == ip) break;
       ip = next;
@@ -147,13 +166,14 @@ function exec_and_split(progA, progB) {
 
 let loop = 0;
 function draw() {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     let untaken = programs.filter(f => f.taken == false);
     let programA = untaken[floor(random(untaken.length))];
-    let neighbourOffsetX = floor(random(-range, range));
-    let neighbourOffsetY = floor(random(-range, range));
-    if (neighbourOffsetX == 0 && neighbourOffsetY == 0) {
-      neighbourOffsetX = 1;
+    let neighbourOffsetX = 0;
+    let neighbourOffsetY = 0;
+    while (neighbourOffsetX == 0 && neighbourOffsetY == 0) {
+      neighbourOffsetX = floor(random(-range, range));
+      neighbourOffsetY = floor(random(-range, range));
     }
     let programB = programAt(
       programA.x + neighbourOffsetX,
@@ -165,6 +185,22 @@ function draw() {
 
     exec_and_split(programA, programB);
   }
+
+  for (let i = 0; i < mutation_amount; i++) {
+    let program = programs[floor(random(programs.length))];
+    let token = program.contents[floor(random(program.contents.length))];
+    if (random() < heavy_mutation_rate) {
+      token.c = floor(random(256));
+    } else {
+      if (random() < 0.5) {
+        token.c = (token.c + 1) % 256;
+      }
+      else {
+        token.c = (token.c - 1 + 256) % 256;
+      }
+    }
+    token.id = token_id++;
+  }
   console.log("loop");
   if (loop++ == 1) {
     for (let i = 0; i < programs.length; i++) {
@@ -174,7 +210,7 @@ function draw() {
       for (let y = 0; y < 8; y++) {
         for (let x = 0; x < 8; x++) {
           let token = program.tokenAt(x, y);
-          if (radio2.value() == 'Instructions') {
+          if (radio_display.value() == 'Instructions') {
             setColorFromChar(token.c, img, offsetX + x, offsetY + y, program.taken);
           } else {
             setColorFromId(token.id, img, offsetX + x, offsetY + y, program.taken);
@@ -195,15 +231,38 @@ function draw() {
 function stringify_bf(arr) {
   return arr.map(a => {
     if ("<>{},.-+[]".indexOf(char(a.c)) == -1) {
-      return a;
+      return a.c;
     }
     return char(a.c);
   });
 }
 
+function matrix_bf(arr) {
+  let str = "<br/>";
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      let c = arr[y * 8 + x].c;
+      str += "<span ";
+      if ("<>{},.-+[]".indexOf(char(c)) == -1) {
+        str += "style='background-color: hsl(0, 0%," + (c / 2.55) + "%)'>&nbsp;";
+      } else {
+        str += "style='background-color: hsl(" + hues[char(c)] + ", 100%, 50%)'>";
+        str += char(c);
+      }
+      str += "</span>";
+    }
+    str += "<br/>";
+  }
+  return str;
+}
+
 function mousePressed() {
   let x = mouseX / (8*pixelSize);
   let y = mouseY / (8*pixelSize);
+  if (x < 0 || y < 0 || x >= w || y >= h) {
+    return;
+  }
   let program = programAt(floor(x), floor(y));
   console.log("Program at ", x, y, stringify_bf(program.contents));
+  programFocus.html('Program at mouse: ' + matrix_bf(program.contents));
 }
